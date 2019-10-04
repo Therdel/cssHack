9d01a1cb17ba08c1360ad3993511c6e5958c5e32
therdel <therdel>
2019-10-04 03:43:14 +0200

    Adds error reporting to linux inject.bash, removes release versions

--- script/inject.bash
+++ script/inject.bash
@@ -6,19 +6,52 @@ pid=$(pidof $process)
 libraryPath=$(realpath "../cmake-build-debug/libcssHack.so")
 library=$(basename $libraryPath)
 
-echo "Process: $process (pid: $(pidof $process))"
+# check running
+if [ -z "$pid" ]
+then
+    echo "$process is not running"
+    exit 1
+fi
+
+echo "Process: $process (pid: $pid)"
 echo "Library: $libraryPath"
 
+# check already injected
 if grep -q $libraryPath /proc/$pid/maps; then
     echo "$library already loaded"
     exit
 fi
 
-sudo gdb -n -q -batch \
-    -ex "attach $pid" \
-    -ex "set \$dlopen = (void*(*)(char*, int)) dlopen" \
-    -ex "call \$dlopen(\"$libraryPath\", 1)" \
-    -ex "detach" \
-    -ex "quit"
+# write gdb script
+echo "attach $pid
+set \$dlopen = (void*(*)(char*, int)) __libc_dlopen_mode
+set \$result = \$dlopen(\"$libraryPath\", 1)
+if \$result == 0
+printf \"Error: %s\\n\", (char*)dlerror()
+else
+print \"Success\"
+end
+detach
+quit
+" > inject.gdb
 
-echo "Injected $library into $process ($pid)"
+# inject
+gdb -q --batch --command=inject.gdb
+
+rm inject.gdb
+
+# check running
+pid=$(pidof $process)
+if [ -z "$pid" ]
+then
+    echo "Injection failed: $process crashed"
+    exit 1
+fi
+
+# check success
+if grep -q $libraryPath /proc/$pid/maps; then
+    echo "Injected $library into $process ($pid)"
+else
+    echo "Injection failed: library not found in process space"
+fi
+
