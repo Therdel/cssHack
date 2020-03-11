0d7ed974dd4015c8a2ee589d254e4fac65f36ae7
Therdel <14974231+Therdel@users.noreply.github.com>
2020-03-11 23:08:50 +0100

    windows onGround signature simplified

--- src/Pointers/Signatures.cpp
+++ src/Pointers/Signatures.cpp
@@ -7,8 +7,8 @@ namespace Signatures {
 	SignatureAOI onGround_op_land(libNames::client, { "74 4E 83 05 ?? ?? ?? ?? 01" }, 2, 7);
 	SignatureAOI onGround_op_leave(libNames::client, { "8b 53 08 83 2d ?? ?? ?? ?? 01" }, 3, 7);
 	#else
-	SignatureAOI onGround(libNames::client, { "85 FF 74 5C FF 05 ?? ?? ?? ?? 85 DB 74 0D" }, 6, 4);
 	SignatureAOI onGround_op_land(libNames::client, { "85 FF 74 5C FF 05 ?? ?? ?? ?? 85 DB 74 0D" }, 4, 6);
 	SignatureAOI onGround_op_leave(libNames::client, { "FF 0D ?? ?? ?? ?? E8 45 3F 15 00" }, 0, 6);
+	SignatureAOI onGround(libNames::client, onGround_op_leave.signature, 2, 4);
 	#endif
 }
